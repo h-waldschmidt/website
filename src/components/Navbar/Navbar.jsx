ad5252bbe26f918f925119bbe61f81ed4a5adad1
Helmut Waldschmidt <helmut.waldschmidt@gmail.com>
2022-02-09 20:26:51 +0100

    added navbar for mobile

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,48 @@
-import React from 'react'
+import React from 'react';
+import { FaBars } from 'react-icons/fa';
+import {
+    Nav,
+    NavbarContainer,
+    NavLogo,
+    MobileIcon,
+    NavMenu,
+    NavItem,
+    NavLinks,
+    NavBtn,
+    NavBtnLink
+} from './NavBarElements';
 
-const Navbar = () => {
+const Navbar = ({ toggle }) => {
     return (
-        <div>
-            Nav
-        </div>
+        <>
+            <Nav>
+                <NavbarContainer>
+                    <NavLogo to='/'>
+                        Helmut
+                    </NavLogo>
+                    <MobileIcon onClick={toggle}>
+                        <FaBars />
+                    </MobileIcon>
+                    <NavMenu>
+                        <NavItem>
+                            <NavLinks to='discover'>Discover</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to='services'>Services</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to='about'>About</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to='signup'>Sign up</NavLinks>
+                        </NavItem>
+                    </NavMenu>
+                    <NavBtn>
+                        <NavBtnLink to='/signin'>Sign In</NavBtnLink>
+                    </NavBtn>
+                </NavbarContainer>
+            </Nav>
+        </>
     )
 }
 
