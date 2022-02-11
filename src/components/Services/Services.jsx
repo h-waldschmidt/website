ea3ad2d98363015527c63b8092918bac4808321c
Helmut Waldschmidt <helmut.waldschmidt@gmail.com>
2022-02-11 21:01:02 +0100

    Added additional site and elements

---
+++ src/components/Services/Services.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import Icon1 from '../../images/svg-1.svg'
+import Icon2 from '../../images/svg-1.svg'
+import Icon3 from '../../images/svg-1.svg'
+import {
+    ServicesContainer,
+    ServicesH1,
+    ServicesWrapper,
+    ServicesCard,
+    ServicesIcon,
+    ServicesH2,
+    ServicesP
+} from './ServicesElements';
+
+const Services = () => {
+    return (
+        <ServicesContainer id='services'>
+            <ServicesH1>Our Services</ServicesH1>
+            <ServicesWrapper>
+                <ServicesCard>
+                    <ServicesIcon src={Icon1} />
+                    <ServicesH2>Reduce Expe</ServicesH2>
+                    <ServicesP>We help</ServicesP>
+                </ServicesCard>
+                <ServicesCard>
+                    <ServicesIcon src={Icon2} />
+                    <ServicesH2>Reduce Expe 2</ServicesH2>
+                    <ServicesP>We help 2</ServicesP>
+                </ServicesCard>
+                <ServicesCard>
+                    <ServicesIcon src={Icon3} />
+                    <ServicesH2>Reduce Expe 3</ServicesH2>
+                    <ServicesP>We help 3</ServicesP>
+                </ServicesCard>
+            </ServicesWrapper>
+        </ServicesContainer>
+    );
+}
+
+export default Services
