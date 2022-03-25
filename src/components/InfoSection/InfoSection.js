4d116b1889935c0bf178a48ca698b871ae268bf7
Helmut Waldschmidt <helmut.waldschmidt@gmail.com>
2022-03-25 11:31:46 +0100

    removed data dependency

--- src/components/InfoSection/InfoSection.js
+++ src/components/InfoSection/InfoSection.js
@@ -14,35 +14,41 @@ import {
     Img
 } from './InfoElements';
 
-function InfoSection({ 
-    lightBg,
-    id,
-    imgStart,
-    lightText,
-    headLine,
-    darkText,
-    description,
-    img,
-    alt,
-    show
-}) 
+function InfoSection() 
 {
 
     return (
         <>
-            <InfoContainer lightBg={lightBg} id={id}>
+            <InfoContainer lightBg={false} id='About'>
                 <InfoWrapper>
-                    <InfoRow imgStart={imgStart}>
+                    <InfoRow imgStart={false}>
                         <Column1>
                             <TextWrapper>
-                                <Heading lightText={lightText}>{headLine}</Heading>
-                                <Subtitle darkText={darkText}>{description}</Subtitle>
-                                <AboutSocials show={show}/>
+                                <Heading lightText={true}>About Me</Heading>
+                                <Subtitle darkText={false}>I'm an Student at Stuttgart University. I am passionate about developing quality software.</Subtitle>
+                                <AboutSocials show={true}/>
                             </TextWrapper>
                         </Column1>
                         <Column2>
                             <ImgWrap>
-                                <Img src={img} alt={alt} />
+                                <Img src={require('../../images/aboutme.svg')} alt='img' />
+                            </ImgWrap>
+                        </Column2>
+                    </InfoRow>
+                </InfoWrapper>
+            </InfoContainer>
+            <InfoContainer lightBg={true} id='Skills'>
+                <InfoWrapper>
+                    <InfoRow imgStart={true}>
+                        <Column1>
+                            <TextWrapper>
+                                <Heading lightText={false}>Skills</Heading>
+                                <Subtitle darkText={true}>Desc</Subtitle>
+                            </TextWrapper>
+                        </Column1>
+                        <Column2>
+                            <ImgWrap>
+                                <Img src={require('../../images/aboutme.svg')} alt='img' />
                             </ImgWrap>
                         </Column2>
                     </InfoRow>
