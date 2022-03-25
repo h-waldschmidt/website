db31438912ab476689fa7cec60f647ba93bf28de
Helmut Waldschmidt <helmut.waldschmidt@gmail.com>
2022-03-25 11:33:22 +0100

    forgot to remove data import

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import HeroSection from '../components/HeroSection/HeroSection';
 import InfoSection from '../components/InfoSection/InfoSection';
-import {
-    homeObjOne,
-    homeObjTwo,
-} from '../components/InfoSection/Data'
 import Services from '../components/Services/Services';
 
 
